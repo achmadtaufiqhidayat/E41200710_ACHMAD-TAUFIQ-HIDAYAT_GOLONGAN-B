fda0bf3bd82c5f954aa96edad166e5ba9c6344bd
achmadtaufiqhidayat <ahmadtaufiqhidayat19@gmail.com>
2021-03-31 16:30:59 +0700

    mengupload latihan dan  tugas pertemuan ke4

--- Tugas&Latihan/TugasMingguKe4/src/TugasB.java
+++ Tugas&Latihan/TugasMingguKe4/src/TugasB.java
@@ -12,21 +12,19 @@ public class TugasB {
     public static void main(String[] args) {
         
         System.out.println("                Do-While                ");
-        System.out.println("Menampilkan Bilangan Genap Antara 0-100 ");
+        System.out.println("Menampilkan Bilangan Genap Antara 1-100 ");
         System.out.println("========================================");
         
         //dekllarasi y
         int y = 1;
-        
+        int x = 100;
+        System.out.print(y + " ");
         
         //memulai perulangan
         do{
         y*=2;
+        System.out.print(y + " ");
         
-        if (y <= 100){
-         System.out.print(y + " ");
-        continue;
-        }
-        }while (y <= 100);
+        }while (y <= x);
     }
 }
